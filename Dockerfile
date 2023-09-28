8df1a5a279b1e33e9fffd2343bbc83286d9ecce3
Adam Labadorf <alabadorf@gmail.com>
2023-09-27 20:21:54 -0400

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM: r-base:4.3.1
+RUN apt update && apt upgrade -y
+COPY install.R /
+RUN Rscript /install.R
---
+++ install.R
@@ -0,0 +1,5 @@
+options(install.packages.check.source = "no")
+install.packages(
+  c("tidyverse", "testthat"),
+  repos="https://cloud.r-project.org"
+)
