ab3e29c4e19cf0a70ca2ef3b0e38ad0304496248
Adam Labadorf <alabadorf@gmail.com>
2023-10-04 22:01:29 -0400

    added knit test for local docker build

--- run_docker_test.sh
+++ run_docker_test.sh
@@ -1,1 +1,5 @@
+# test test
 docker run --rm -v $PWD/workspace:/github/workspace bf591-r-docker test main.R test_main.R test_main.out
+
+# test knit
+docker run --rm -v $PWD/workspace:/github/workspace bf591-r-docker knit main.R report.Rmd reference_report.html rmarkdown_build.out
