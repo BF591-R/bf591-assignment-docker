708b3d1ef2d6b219823c6c14c82fba79fb101b41
Adam Labadorf <alabadorf@gmail.com>
2023-10-12 10:40:51 -0400

    fixed entrypoint usage messages, updated to support test_results.csv

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-if [ -z "${1}" ] || [ -z "${2}" ] || [ -z "${3}" ];
+if [ -z "${1}" ];
 then
     echo "Usage: entrypoint.sh (test|knit)"
     exit 1
@@ -7,16 +7,16 @@ fi
 cd /github/workspace
 case "${1}" in
     test)
-        if [ -z "${2}" ] || [ -z "${3}" ];
+        if [ -z "${2}" ] || [ -z "${3}" ] || [ -z "${4}" ];
         then
-            echo "Usage: entrypoint.sh test <main fn> <test fn>"
+            echo "Usage: entrypoint.sh test <main fn> <test fn> <test results out fn>"
             exit 1
         fi
 
-        Rscript /test.R $2 $3
+        Rscript /test.R $2 $3 $4
         ;;
     knit)
-        if [ -z "${2}" ] || [ -z "${3}" ];
+        if [ -z "${2}" ] || [ -z "${3}" ] || [ -z "${4}" ];
         then
             echo "Usage: entrypoint.sh knit <main fn> <report fn> <out html fn>"
             exit 1
@@ -25,7 +25,7 @@ case "${1}" in
         Rscript /knit.R $2 $3 $4
         ;;
     *)
-        echo "Usage: entrypoint.sh (test|knit) <fn> <outfn>"
+        echo "Usage: entrypoint.sh (test|knit)"
         exit 1
         ;;
 esac
